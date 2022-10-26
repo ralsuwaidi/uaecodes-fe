dde801dc1f4a6bab78dab8ababba8daf51291949
rashed <x.suwaidi@gmail.com>
2022-10-26 13:44:50 +0400

    add matrix

--- src/lineofcode/index.js
+++ src/lineofcode/index.js
@@ -4,8 +4,16 @@ import TextEditor from "./TextEditor";
 import "react-tiny-fab/dist/styles.css";
 import { Fab, Action } from "react-tiny-fab";
 import { MdPlayArrow, MdHelp, MdAdd } from "react-icons/md";
+import { useState } from "react";
+import Typewriter from "typewriter-effect";
+import MatrixCard from "matrix-card";
 
 function LineOfCode() {
+  const [console, setConsole] = useState(
+    "Console loaded...Type your script above and click green play button"
+  );
+
+
   return (
     <div className="LOC flex flex-col h-screen">
       <Fab
@@ -15,11 +23,13 @@ function LineOfCode() {
         style={{ right: 15, bottom: 95, width: 35, height: 35 }} // location
         mainButtonStyles={{ backgroundColor: "gray", width: 35, height: 35 }}
       >
-        <Action text="Email" 
-        style={{ backgroundColor: "gray", width: 35, height: 35 }}
+        <Action
+          text="Email"
+          style={{ backgroundColor: "gray", width: 35, height: 35 }}
         />
-        <Action text="Help"
-        style={{ backgroundColor: "gray", width: 35, height: 35 }}
+        <Action
+          text="Help"
+          style={{ backgroundColor: "gray", width: 35, height: 35 }}
         >
           <MdHelp />
         </Action>
@@ -31,8 +41,8 @@ function LineOfCode() {
         event="click"
         style={{ right: 5, bottom: 24 }} // location
         mainButtonStyles={{ backgroundColor: "lime" }}
-        onClick={() => console.log("Here is the action of FAB.")}
-        ></Fab>
+        onClick={() => setConsole("Your function has beeen printed")}
+      ></Fab>
 
       <Header />
       <TextEditor />
@@ -40,13 +50,34 @@ function LineOfCode() {
         style={{ backgroundColor: "#242424" }}
         className="flex-1 h-full flex-grow pl-2 loc-mono text-sm Green"
       >
-        <p>
-          <span>&#62;</span> Console loaded...Type your script above and click
-          green start button
-        </p>
-        <p>
-          <span>&#62;</span> |
-        </p>
+        <div className="w-full">
+          <MatrixCard
+            id={"my-id-1"}
+            matrixText={"UAE CODES"}
+            canvasSize={{ width: '900', height: '300' }}
+            delay={40}
+            backgroundColor={"rgba(42, 40, 45, 0.2)"}
+            textFontSize={"16"}
+            textMainColor={"#A9A9A9"}
+            textAlternateColorRatio={0.1}
+            textAlternateColorList={[
+              "#808080",
+              "#989898",
+              "#A9A9A9",
+              "#C0C0C0",
+            ]}
+            styleOverrideForChildrenDiv={{ "background-color": "transparent", "top": "0px", "margin": "0px", "padding": "0px", "left": "0px" }}
+          >
+            <div>
+              <Typewriter
+                options={{
+                  strings: [">> " + console],
+                  autoStart: true,
+                }}
+              />
+              </div>
+          </MatrixCard>
+        </div>
       </div>
     </div>
   );
